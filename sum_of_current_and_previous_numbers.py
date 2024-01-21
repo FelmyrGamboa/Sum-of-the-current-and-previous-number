0268aa23b3dbc50522e141634f6ea1c400515aff
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-21 23:43:09 +0800

    Assigning conditions inside the for loop: sum and adjusting the numbers

--- sum_of_current_and_previous_numbers.py
+++ sum_of_current_and_previous_numbers.py
@@ -23,5 +23,7 @@ previous_number = starting_number
 #Create a for loop with a range of 10 using i as current number's value
 for i in range(10):
 #Calculate for the sum of the previous number with the i as current number
+    sum = previous_number + i
 #Change the previous number to be the current number 
+    previous_number = i
 #Display the result
